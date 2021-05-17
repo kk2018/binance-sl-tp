9ca4e538f505f90973ad1aa407d51b8ca9df231b
Nadia <rebinnaf@yahoo.com>
2021-05-18 02:22:28 +0430

    clean and fix package update error

--- futures_websocket.py
+++ futures_websocket.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import threading
 from binance.client import Client
-from binance.websockets import BinanceSocketManager
+from binance import BinanceSocketManager
 
 
 class FuturesClient(Client):
@@ -12,42 +12,38 @@ class FuturesClient(Client):
         return self._request(method, uri, signed, True, **kwargs)
 
     def futures_stream_get_listen_key(self):
-        res = self._request_futures_api(
-            'post', 'listenKey', signed=False, data={})
-        return res['listenKey']
+        res = self._request_futures_api("post", "listenKey", signed=False, data={})
+        return res["listenKey"]
 
     def futures_stream_keepalive(self, listenKey):
-        params = {
-            'listenKey': listenKey
-        }
-        return self._request_futures_api('put', 'listenKey', signed=False, data=params)
+        params = {"listenKey": listenKey}
+        return self._request_futures_api("put", "listenKey", signed=False, data=params)
 
     def futures_stream_close(self, listenKey):
 
-        params = {
-            'listenKey': listenKey
-        }
-        return self._request_futures_api('delete', 'listenKey', signed=False, data=params)
+        params = {"listenKey": listenKey}
+        return self._request_futures_api(
+            "delete", "listenKey", signed=False, data=params
+        )
 
 
 class FuturesWebsockets(BinanceSocketManager):
-    STREAM_URL = 'wss://stream.binance.com:9443/'
-    FSTREAM_URL = 'wss://fstream.binance.com/'
-    VSTREAM_URL = 'wss://vstream.binance.com/'
-    VSTREAM_TESTNET_URL = 'wss://testnetws.binanceops.com/'
+    STREAM_URL = "wss://stream.binance.com:9443/"
+    FSTREAM_URL = "wss://fstream.binance.com/"
+    VSTREAM_URL = "wss://vstream.binance.com/"
+    VSTREAM_TESTNET_URL = "wss://testnetws.binanceops.com/"
 
-    WEBSOCKET_DEPTH_5 = '5'
-    WEBSOCKET_DEPTH_10 = '10'
-    WEBSOCKET_DEPTH_20 = '20'
+    WEBSOCKET_DEPTH_5 = "5"
+    WEBSOCKET_DEPTH_10 = "10"
+    WEBSOCKET_DEPTH_20 = "20"
 
     DEFAULT_USER_TIMEOUT = 30 * 60  # 30 minutes
 
     def __init__(self, client, user_timeout=DEFAULT_USER_TIMEOUT):
         super().__init__(client, user_timeout)
-        self._timers = {'user': None, 'margin': None, 'futures': None}
-        self._listen_keys = {'user': None, 'margin': None, 'futures': None}
-        self._account_callbacks = {
-            'user': None, 'margin': None, 'futures': None}
+        self._timers = {"user": None, "margin": None, "futures": None}
+        self._listen_keys = {"user": None, "margin": None, "futures": None}
+        self._account_callbacks = {"user": None, "margin": None, "futures": None}
 
     def start_futures_user_socket(self, callback):
         """Start a websocket for user data
@@ -61,7 +57,7 @@ class FuturesWebsockets(BinanceSocketManager):
         # Get the user listen key
         user_listen_key = self._client.futures_stream_get_listen_key()
         # and start the socket with this specific key
-        return self._start_account_futures_socket('futures', user_listen_key, callback)
+        return self._start_account_futures_socket("futures", user_listen_key, callback)
 
     def _start_account_futures_socket(self, socket_type, listen_key, callback):
         """Starts futures account socket"""
@@ -75,14 +71,14 @@ class FuturesWebsockets(BinanceSocketManager):
         return conn_key
 
     def _keepalive_account_socket(self, socket_type):
-        if socket_type == 'user':
+        if socket_type == "user":
             callback = self._account_callbacks[socket_type]
             listen_key = self._client.stream_get_listen_key()
-        elif socket_type == 'margin':  # cross-margin
+        elif socket_type == "margin":  # cross-margin
             listen_key_func = self._client.margin_stream_get_listen_key
             callback = self._account_callbacks[socket_type]
             listen_key = listen_key_func()
-        elif socket_type == 'futures':
+        elif socket_type == "futures":
             listen_key_func = self._client.futures_stream_get_listen_key
             callback = self._account_callbacks[socket_type]
             listen_key = self._client.margin_stream_get_listen_key()
@@ -91,4 +87,5 @@ class FuturesWebsockets(BinanceSocketManager):
             listen_key_func = self._client.isolated_margin_stream_get_listen_key
             callback = self._account_callbacks.get(socket_type, None)
             listen_key = self._client.isolated_margin_stream_get_listen_key(
-                socket_type)  # Passing symbol for isolated margin
+                socket_type
+            )  # Passing symbol for isolated margin
